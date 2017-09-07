ee3bfc637c61868ca867922c8f3d7451e764a655
Brendan Forster <brendan@github.com>
2017-09-07 14:14:20 +1000

    adding a test for failing in a given mode

--- vcr-sharp-tests/ReplayingHandler.cs
+++ vcr-sharp-tests/ReplayingHandler.cs
@@ -1,44 +1,86 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace VcrSharp.Tests
 {
+    public enum VCRMode
+    {
+        /// <summary>
+        /// Only use the local fixtures when executing network requests
+        /// </summary>
+        Playback,
+        /// <summary>
+        /// Use the cached response if found, otherwise fetch and store the result
+        /// </summary>
+        Cache,
+        /// <summary>
+        /// Avoid cached responses - use the network and store the result
+        /// </summary>
+        Record
+    }
+
     public class ReplayingHandler : DelegatingHandler
-{
-    private readonly Cassette cassette;
-
-    public ReplayingHandler(HttpMessageHandler innerHandler, string cassettePath) : base(innerHandler)
     {
-        cassette = new Cassette(cassettePath);
-    }
+        private readonly Cassette cassette;
 
-    public ReplayingHandler(string cassettePath) : this(new HttpClientHandler(), cassettePath)
-    {
-
-    }
-
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        var cachedResponse = await cassette.findCachedResponseAsync(request);
-
-        if (cachedResponse.Item1)
+        public ReplayingHandler(HttpMessageHandler innerHandler, string cassettePath) : base(innerHandler)
         {
-            return cachedResponse.Item2;
+            cassette = new Cassette(cassettePath);
         }
 
-        var freshResponse = await base.SendAsync(request, cancellationToken);
+        public ReplayingHandler(string cassettePath) : this(new HttpClientHandler(), cassettePath)
+        {
 
-        await cassette.storeCachedResponseAsync(request, freshResponse);
+        }
 
-        return freshResponse;
+        VCRMode CurrentVCRMode
+        {
+            get
+            {
+                var mode = Environment.GetEnvironmentVariable("VCR_MODE");
+
+                if (string.IsNullOrWhiteSpace(mode))
+                {
+                    return VCRMode.Playback;
+                }
+
+                if (Enum.TryParse(mode, out VCRMode result))
+                {
+                    return result;
+                }
+
+                return VCRMode.Playback;
+            }
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var cachedResponse = await cassette.findCachedResponseAsync(request);
+
+            if (cachedResponse.Item1)
+            {
+                return cachedResponse.Item2;
+            }
+
+            if (CurrentVCRMode == VCRMode.Playback)
+            {
+                throw new PlaybackException("A cached response was not found, and the environment is in playback mode which means the network cannot be accessed.");
+            }
+
+            var freshResponse = await base.SendAsync(request, cancellationToken);
+
+            await cassette.storeCachedResponseAsync(request, freshResponse);
+
+            return freshResponse;
+        }
+
+        protected override async void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            await cassette.FlushToDisk();
+        }
     }
-
-    protected override async void Dispose(bool disposing)
-    {
-        base.Dispose(disposing);
-
-        await cassette.FlushToDisk();
-    }
-}
 }
--- vcr-sharp-tests/ScratchPad.cs
+++ vcr-sharp-tests/ScratchPad.cs
@@ -1,21 +1,55 @@
 using Shouldly;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace VcrSharp.Tests
 {
-    public class ScratchPad
+    public class ScratchPad : IDisposable
     {
         [Fact]
-        public async Task ScratchpadUsesGivenCassette()
+        public async Task UseTheLocalFixtureToRetrieveTheResponse()
         {
             using (var httpClient = HttpClientFactory.WithCassette("example-test"))
             {
+                Environment.SetEnvironmentVariable("VCR_MODE", "playback");
+
                 var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, "http://www.iana.org/domains/reserved"));
                 var body = await response.Content.ReadAsStringAsync();
                 body.ShouldContain("Example domains");
             }
+        }
+
+        [Fact]
+        public async Task ErrorsWhenTheRequestIsNotInTheCache()
+        {
+            using (var httpClient = HttpClientFactory.WithCassette("no-cache-defined"))
+            {
+                Environment.SetEnvironmentVariable("VCR_MODE", "playback");
+
+                await Assert.ThrowsAsync<PlaybackException>(() => httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, "http://www.iana.org/domains/reserved")));
+            }
+        }
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Environment.SetEnvironmentVariable("VCR_MODE", "");
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
         }
     }
 }
